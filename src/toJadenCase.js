4a9003e32b30931d4571ad0b5896f86d3d4e328a
Aljosa Janjic <janjic_aljosa@hotmail.com>
2023-08-14 08:07:26 +0200

    Regex works locally

--- src/toJadenCase.js
+++ src/toJadenCase.js
@@ -1,20 +1,24 @@
 function toJadenCase(str) {
 
-  // console.log(typeof str)
-  // if (typeof str !== 'string') {
-  //   return
-  // }
+  console.log(typeof str)
+  if (typeof str !== 'string') {
+    return
+  }
 
-  // if (str.length === 0 ) return str;
+  if (str.length === 0 ) return str;
 
-  // let result = str
-  //   .split(' ')
-  //   .map( word => word[0].toUpperCase() + word.slice(1))
-  //   .join(' ')
+  let result = str
+    .split(' ')
+    .map( word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ')
 
-  // return result;
+  return result;
 
-  
+  // if (typeof str !== 'string') return;
+
+  // return str.replace(/\b[a-z]/g, (letter) => {
+  //   return letter.toUpperCase()
+  // })
 
 }
 
